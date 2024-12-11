c88f403b2d865794d44f44e09ffd24b08ef0c7b4
Raghu K <raghuk.vit@gmail.com>
2024-12-11 07:16:40 +0530

    RoboSHop Project

--- common.sh
+++ common.sh
@@ -1,28 +1,32 @@
 dir_path=$(pwd)
+log_file=/tmp/roboshop.log
+
 
 SYSTEMD_SETUP() {
-  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service
-  systemctl daemon-reload
-  systemctl enable $app_name
-  systemctl restart $app_name
+  cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service >$log_file
+  systemctl daemon-reload >$log_file
+  systemctl enable $app_name >$log_file
+  systemctl restart $app_name >$log_file
 }
 
 APP_PREREQ() {
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-  cd /app
-  unzip /tmp/$app_name.zip
+  useradd roboshop >$log_file
+  rm -rf /app >$log_file
+  mkdir /app >$log_file
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip >$log_file
+  cd /app >$log_file
+  unzip /tmp/$app_name.zip >$log_file
 }
 
 NODEJS() {
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
+  echo Disable Default NodeJS Version
+  dnf module disable nodejs -y >$log_file
+  dnf module enable nodejs:20 -y >$log_file
+  dnf install nodejs -y >$log_file
 
   APP_PREREQ
-  npm install
+
+  npm install >$log_file
 
   SYSTEMD_SETUP
 
