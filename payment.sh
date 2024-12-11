e8ae163ed50244d062eeb89d50cb3fa2aaa19824
Raghu K <raghuk.vit@gmail.com>
2024-12-11 06:25:39 +0530

    RoboSHop Project

--- payment.sh
+++ payment.sh
@@ -2,6 +2,8 @@ cp payment.service /etc/systemd/system/payment.service
 
 dnf install python3 gcc python3-devel -y
 useradd roboshop
+
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
 cd /app
