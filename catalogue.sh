045893484788cc0052bc69c7b901792db3f12ce0
Raghu K <raghuk.vit@gmail.com>
2024-12-12 06:07:46 +0530

    RoboSHop Project

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ app_name=catalogue
 
 NODEJS
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp $dir_path/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.azdevopsb82.online </app/db/master-data.js
 
