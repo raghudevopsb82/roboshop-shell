af602c8b3b7251e1396d40368dc18242ad2be86b
Raghu K <raghuk.vit@gmail.com>
2024-12-12 06:29:15 +0530

    RoboSHop Project

--- common.sh
+++ common.sh
@@ -6,47 +6,58 @@ rm -f $log_file
 SYSTEMD_SETUP() {
   echo Copy Application Service File
   cp $dir_path/$app_name.service /etc/systemd/system/$app_name.service &>>$log_file
+  echo $?
 
   echo Start Application
   systemctl daemon-reload &>>$log_file
   systemctl enable $app_name &>>$log_file
   systemctl restart $app_name &>>$log_file
+  echo $?
 }
 
 APP_PREREQ() {
 
   echo Create Application User
   useradd roboshop &>>$log_file
+  echo $?
 
   echo Remove Application Directory
   rm -rf /app &>>$log_file
+  echo $?
 
   echo Create Application Directory
   mkdir /app &>>$log_file
+  echo $?
 
   echo Download Application Code
   curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
+  echo $?
 
   cd /app &>>$log_file
 
   echo Extract Application Code
   unzip /tmp/$app_name.zip &>>$log_file
+  echo $?
 }
 
 NODEJS() {
   echo Disable Default NodeJS Version
   dnf module disable nodejs -y &>>$log_file
+  echo $?
 
   echo Enable NodeJS 20 Version
   dnf module enable nodejs:20 -y &>>$log_file
+  echo $?
 
   echo Install NodeJS
   dnf install nodejs -y &>>$log_file
+  echo $?
 
   APP_PREREQ
 
   echo Install NodeJS Dependencies
   npm install &>>$log_file
+  echo $?
 
   SYSTEMD_SETUP
 
